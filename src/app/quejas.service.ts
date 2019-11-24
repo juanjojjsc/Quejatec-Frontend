66498f5230d0d6e0b04c11351ba47eac6cfcc8f6
JJ Santos <jj@HAL-9031.local>
2019-11-24 16:02:27 -0600

    VerQueja

--- src/app/quejas.service.ts
+++ src/app/quejas.service.ts
@@ -51,6 +51,45 @@ export class QuejasService {
       });
   }
 
+  getQueja(id: string) {
+
+    console.log('http://10.0.1.70:3000/client/issue/' + id);
+    this.http
+      .get<any[]>(
+        'http://10.0.1.70:3000/client/issue/' + id
+      )
+      .pipe(map((quejaData) => {
+        console.log(quejaData);
+        return quejaData;
+        //        return quejaData.issues.map(quejaR => {
+
+          /*
+          return {
+            _id: quejaR._id,
+            uId: quejaR.uId,
+            placeEvent: quejaR.placeEvent,
+            description: quejaR.description,
+            status: quejaR.status,
+            userRate: quejaR.userRate,
+            answers: quejaR.answers,
+            finalComment: quejaR.finalComment,
+            registerDate: quejaR.registerDate
+          };
+          */
+        //});
+      }))
+      .subscribe(transformedQuejas => {
+        console.log(transformedQuejas);
+
+        this.quejas = transformedQuejas;
+        //this.quejasUpdated.next([...this.quejas]);
+
+        this.quejasUpdated.next(this.quejas);
+
+
+      });
+  }
+
   getQuejaUpdateListener() {
     return this.quejasUpdated.asObservable();
   }
--- src/app/verqueja/verqueja.component.ts
+++ src/app/verqueja/verqueja.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Queja } from '../queja.model';
+import { Subscription } from 'rxjs';
+import { QuejasService } from '../quejas.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-verqueja',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerquejaComponent implements OnInit {
 
-  constructor() { }
+  quejas: Queja[] = [];
+
+  quejaId: string;
+
+  private quejasSub: Subscription;
+
+  constructor(public quejasSerivice: QuejasService, private activatedRoute: ActivatedRoute) {
+
+  }
 
   ngOnInit() {
+
+    this.quejaId = this.activatedRoute.snapshot.paramMap.get('id');
+
+    //this.quejaId = "5dcd6fed9baf9b3a9cb10921";
+
+
+    this.quejasSerivice.getQueja(this.quejaId);
+
+    this.quejasSub = this.quejasSerivice.getQuejaUpdateListener()
+    .subscribe((quejas: Queja[]) => {
+      console.log("FETCHED", quejas);
+      this.quejas = quejas;
+
+    });
+
   }
 
 }
